8bf4b498880195f268953655b500d107562e0edd
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-07-04 21:30:50 +0300

    доп задание иправленное 2 задание

--- task_2.py
+++ task_2.py
@@ -1,4 +1,7 @@
 _list1 = [1, 2, 3]
 _list2 = [11, 22, 33]
-_list3 = _list1 + _list2
+_list3 = []
+for i in range(len(_list1)):
+    _list3.append(_list1[i])
+    _list3.append(_list2[i])
 print(_list3)
